d21b8b427c64bce1b80117bfa20f6f3d677399db
vad1m198 <persidskij_on_ferrari@ukr.net>
2016-11-30 13:23:32 +0200

    1.4 JUnit

--- chapter_001/src/main/java/ru/vmerkotan/Calculate.java
+++ chapter_001/src/main/java/ru/vmerkotan/Calculate.java
@@ -1,5 +1,10 @@
 package ru.vmerkotan;
 
+/**
+* Class Calculate решение задачи части 001 урок1.
+* @author Vadim Merkotans
+* @since  1.0
+*/
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
---
+++ chapter_001/src/test/java/ru/vmerkotan/CalculateTest.java
@@ -0,0 +1,28 @@
+package ru.vmerkotan;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Tests for Calculate class
+* 
+* @author Vadim Merkotans
+* @since  1.0
+* @version $Id$
+*/
+public class CalculateTest {
+	
+	/**
+	* Test add.
+	*/
+	@Test
+	public void whenExecuteMainThenPrintToConsole() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(out.toString(), is("Hello World!\r\n"));
+	}
+}
