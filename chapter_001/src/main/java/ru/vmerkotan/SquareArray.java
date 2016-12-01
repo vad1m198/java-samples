eaf89bd358803162bac79dfdbb7a0761baccdab7
vad1m198 <persidskij_on_ferrari@ukr.net>
2016-12-01 19:25:21 +0200

    5.2 square array class was created and test class was created. TODO add implementation

---
+++ chapter_001/src/main/java/ru/vmerkotan/SquareArray.java
@@ -0,0 +1,18 @@
+package ru.vmerkotan;
+
+/**
+* The {@code SquareArray} class
+* @author Vadim Merkotan
+* @since  1.0
+*/
+public class SquareArray {
+	
+	/**
+	* turns array by 90 degrees clockwise.
+	* Works only with square arrays
+	*/
+	public int[][] turn(int[][] array) {
+		return array;
+	}
+	
+}
---
+++ chapter_001/src/test/java/ru/vmerkotan/SquareArrayTest.java
@@ -0,0 +1,30 @@
+package ru.vmerkotan;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+/**
+* Tests for SquareArray class
+* @author Vadim Merkotan
+* @since  1.0
+*/
+public class SquareArrayTest {
+	
+	/**
+	* test turn method.
+	*/
+	@Test
+	public void test() {
+		SquareArray sa = new SquareArray();
+		int[][] testArray = new int[][]{
+		  { 1, 1 },
+		  { 0, 0 }
+		};
+		int[][] expectedArray = new int[][]{
+		  { 0, 1 },
+		  { 0, 1 }
+		};		
+		assertThat(expectedArray, is(sa.turn(testArray)));
+	}
+}
