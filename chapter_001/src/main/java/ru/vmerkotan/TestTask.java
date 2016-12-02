90a049b621508e39fbb0666a95260488546b0d82
vad1m198 <persidskij_on_ferrari@ukr.net>
2016-12-02 20:17:48 +0200

    6.0 test task failed tests

---
+++ chapter_001/src/main/java/ru/vmerkotan/TestTask.java
@@ -0,0 +1,43 @@
+package ru.vmerkotan;
+
+import java.util.Arrays;
+/**
+* The {@code TestTask} class represent
+* this final task for chapter001
+* @author Vadim Merkotan
+* @since  1.0
+*/
+public class TestTask {
+	
+	/**
+	* Verifies that origin string contains
+	* sub String
+	* @param origin	String to check with
+	* @param sub	String to check what
+	* @return		true if origin contains sub else return false
+	*/
+	public boolean contains(String origin, String sub) {		
+		char[] originArray = origin.toCharArray();
+		char[] subArray = sub.toCharArray();
+		if(subArray.length > originArray.length) {
+			return false;
+		}
+		
+		for(int i = 0; i < originArray.length; i++) {
+			if(originArray[i] == subArray[0]) {
+				boolean result = true;
+				char[] originSubArray = Arrays.copyOfRange(originArray, i, subArray.length);
+				for(int j = 0; j < subArray.length; j++) {
+					if(subArray[j] != originSubArray[j]) {
+						result = false;
+					}
+				}
+				if(result) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}	
+}
---
+++ chapter_001/src/test/java/ru/vmerkotan/TestTaskTest.java
@@ -0,0 +1,73 @@
+package ru.vmerkotan;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+/**
+* Tests for TestTask class
+* @author Vadim Merkotan
+* @since  1.0
+*/
+public class TestTaskTest {
+	
+	/**
+	* test contains method. Should return true
+	* if origin string contains sub
+	*/
+	@Test
+	public void whenOriginContainsSubThenReturnTrue() {
+		TestTask task = new TestTask();		
+		assertEquals(true, task.contains("Hello123", "ello"));
+	}
+	
+	/**
+	* test contains method. Should return false
+	* if origin string contains only part sub
+	*/
+	@Test
+	public void whenOriginContainsPartSubThenReturnFalse() {
+		TestTask task = new TestTask();		
+		assertEquals(false, task.contains("Hello123", "elloM"));
+	}
+	
+	/**
+	* test contains method. Should return false
+	* if origin contains sub but with gaps return false
+	*/
+	@Test
+	public void whenOriginContainsSubWithGapsThenReturnFalse() {
+		TestTask task = new TestTask();		
+		assertEquals(false, task.contains("Hello123", "elo"));
+	}
+	
+	/**
+	* test contains method. Should return true
+	* if origin string contains sub and starts with sub
+	*/
+	@Test
+	public void whenOriginContainsAndStartsWithSubThenReturnTrue() {
+		TestTask task = new TestTask();		
+		assertEquals(true, task.contains("Hello123", "Hell"));
+	}
+	
+	/**
+	* test contains method. Should return true
+	* if origin string contains sub and ends with sub
+	*/
+	@Test
+	public void whenOriginContainsAndEndsWithSubThenReturnTrue() {
+		TestTask task = new TestTask();		
+		assertEquals(true, task.contains("Hello123", "123"));
+	}
+	
+	/**
+	* test contains method. Should return true
+	* if origin string contains multi sub
+	*/
+	@Test
+	public void whenOriginContainsMultiSubThenReturnTrue() {
+		TestTask task = new TestTask();		
+		assertEquals(true, task.contains("123Hello123", "123"));
+	}
+}
