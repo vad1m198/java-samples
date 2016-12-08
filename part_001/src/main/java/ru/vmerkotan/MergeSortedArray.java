e90bee3c2fcee9a8fb5ca5a9901973e1f9e9ae91
vad1m198 <persidskij_on_ferrari@ukr.net>
2016-12-08 12:33:12 +0200

    4.4 merge sorted arrays

---
+++ part_001/src/main/java/ru/vmerkotan/MergeSortedArray.java
@@ -0,0 +1,34 @@
+package ru.vmerkotan;
+
+/**
+* The {@code MergeSortedArray} class
+* @author Vadim Merkotan
+* @since  1.0
+*/
+public class MergeSortedArray {
+	
+	/**
+	* merges given sorted arrays in one
+	* work only with ascending sorted arrays
+	*
+	* @param first	first sorted array
+	* @param second	second sorted array
+	* @return		merged sorted ascending array
+	*/
+	public int[] merge(int[] first, int[] second) {
+		int [] result = new int[first.length + second.length];
+		int firstArrPointer = 0;
+		int secondArrPointer = 0;
+		for(int i = 0; i < result.length; i++) {
+			if(firstArrPointer == first.length) {
+				result[i] = second[secondArrPointer++];
+			} else if(secondArrPointer == second.length) {
+				result[i] = first[firstArrPointer++];
+			} else {
+				result[i] = first[firstArrPointer] > second[secondArrPointer] 
+					? second[secondArrPointer++] : first[firstArrPointer++];
+			}			
+		}
+		return result;
+	}
+}
---
+++ part_001/src/test/java/ru/vmerkotan/MergeSortedArrayTest.java
@@ -0,0 +1,53 @@
+package ru.vmerkotan;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+/**
+* Tests for MergeSortedArray class
+* @author Vadim Merkotan
+* @since  1.0
+*/
+public class MergeSortedArrayTest {
+	
+	/**
+	* test merge method.
+	* params should be ascending sorted
+	*/
+	@Test
+	public void whenPassSortedArraysThenMergedSortedArrayShouldBeReturned() {
+		MergeSortedArray msa = new MergeSortedArray();
+		int[] first = new int[]{1,3,5,7};
+		int[] second = new int[]{2,4,6,8};
+		assertThat(new int[]{1,2,3,4,5,6,7,8}, is(msa.merge(first, second)));
+	}
+	
+	/**
+	* test merge method.
+	* params should be ascending sorted
+	* If pass sorted arrays with disjoint numbers
+	* method should work 
+	*/
+	@Test
+	public void whenDisjointArraysThenMergedappropriate() {
+		MergeSortedArray msa = new MergeSortedArray();
+		int[] first = new int[]{1,3,5,7};
+		int[] second = new int[]{9,10,11,15};
+		assertThat(new int[]{1,3,5,7,9,10,11,15}, is(msa.merge(first, second)));
+	}
+	
+	/**
+	* test merge method.
+	* params should be ascending sorted
+	* Test with negative numbers
+	*/
+	@Test
+	public void whenPassNegativeThenMergeApropriate() {
+		MergeSortedArray msa = new MergeSortedArray();
+		int[] first = new int[]{-3,-1,5,7};
+		int[] second = new int[]{-2,0,4,15};
+		assertThat(new int[]{-3,-2,-1,0,4,5,7,15}, is(msa.merge(first, second)));
+	}
+	
+}
