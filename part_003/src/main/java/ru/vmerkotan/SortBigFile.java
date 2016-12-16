5e7e15e7a6142866e1d1bb03fb6b9d15277afdb5
vad1m198 <persidskij_on_ferrari@ukr.net>
2016-12-16 17:36:45 +0200

    external sort sort

--- part_003/src/main/java/ru/vmerkotan/SortBigFile.java
+++ part_003/src/main/java/ru/vmerkotan/SortBigFile.java
@@ -23,37 +23,70 @@ public class SortBigFile {
      */
     public void sort(File source, File dest) throws IOException {
 
-        int LINES_CAPACITY = 10;
+        final int LINES_CAPACITY = 10;
 
         dest.getParentFile().mkdirs();
         dest.createNewFile();
 
+        File tmp1 = new File(dest.getParentFile().getAbsolutePath() + File.separator + "tmp1.txt");
+        File tmp2 = new File(dest.getParentFile().getAbsolutePath() + File.separator + "tmp2.txt");
+        File tmp3 = new File(dest.getParentFile().getAbsolutePath() + File.separator + "tmp3.txt");
+        File tmp4 = new File(dest.getParentFile().getAbsolutePath() + File.separator + "tmp4.txt");
+        tmp1.createNewFile();
+        tmp2.createNewFile();
+        tmp3.createNewFile();
+        tmp4.createNewFile();
+
         RandomAccessFile raf = new RandomAccessFile(source, "r");
         RandomAccessFile rafOut = new RandomAccessFile(dest, "rw");
+        RandomAccessFile rafTmp1 = new RandomAccessFile(tmp1, "rw");
+        RandomAccessFile rafTmp2 = new RandomAccessFile(tmp2, "rw");
+        RandomAccessFile rafTmp3 = new RandomAccessFile(tmp3, "rw");
+        RandomAccessFile rafTmp4 = new RandomAccessFile(tmp4, "rw");
         List<String> lines = new ArrayList<>();
 
         String str;
         int counter = LINES_CAPACITY;
+        boolean writeFirst = false;
         while((str = raf.readLine()) != null) {
             lines.add(str);
-
             if(--counter == 0 || raf.getFilePointer() == raf.length()) {
                 lines.sort(new CompareStringsByLength());
                 for(String s: lines) {
-                    rafOut.writeBytes(s + System.getProperty("line.separator"));
+                    if(writeFirst) {
+                        rafTmp1.writeBytes(s + System.getProperty("line.separator"));
+                    } else {
+                        rafTmp2.writeBytes(s + System.getProperty("line.separator"));
+                    }
                 }
+                writeFirst = !writeFirst;
                 lines.clear();
                 counter = LINES_CAPACITY;
             }
         }
-/*
-        lines.sort(new CompareStringsByLength());
 
-        for(String s: lines) {
-            rafOut.writeBytes(s + System.getProperty("line.separator"));
-        }*/
+        rafTmp1.seek(0);
+        rafTmp2.seek(0);
+
+        System.out.println(rafTmp1.length() + ":" + rafTmp2.length());
+        while((str = rafTmp1.readLine()) != null) {
+            rafOut.writeBytes( str + System.getProperty("line.separator"));
+        }
+
+        while((str = rafTmp2.readLine()) != null) {
+            rafOut.writeBytes( str + System.getProperty("line.separator"));
+        }
+
+        rafTmp1.close();
+        rafTmp2.close();
+        rafTmp3.close();
+        rafTmp4.close();
         raf.close();
         rafOut.close();
+//        tmp1.delete();
+//        tmp2.delete();
+//        tmp3.delete();
+//        tmp4.delete();
     }
 
     private class CompareStringsByLength implements Comparator<String> {
