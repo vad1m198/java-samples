15290c19f13e32465b33cde7d9defc837c4a86e7
vad1m198 <persidskij_on_ferrari@ukr.net>
2016-12-16 11:33:29 +0200

    drop abuse add try with resources

--- part_003/src/main/java/ru/vmerkotan/DropAbuse.java
+++ part_003/src/main/java/ru/vmerkotan/DropAbuse.java
@@ -3,7 +3,6 @@ package ru.vmerkotan;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
@@ -11,19 +10,19 @@ import java.util.Scanner;
  */
 public class DropAbuse {
 
-    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
-        Scanner sc  = new Scanner(in);
+    public void dropAbuses(InputStream inStream, OutputStream outStream, String[] abuse) throws IOException {
 
-        while(sc.hasNextLine()) {
-            String s = sc.nextLine();
-            for (String str : abuse) {
-                s = s.replaceAll(str, "");
+        try (InputStream in = inStream; OutputStream out = outStream) {
+            Scanner sc  = new Scanner(in);
+            while(sc.hasNextLine()) {
+                String s = sc.nextLine();
+                for (String str : abuse) {
+                    s = s.replaceAll(str, "");
+                }
+                out.write(s.getBytes());
+                out.flush();
             }
-            out.write(s.getBytes());
-            out.flush();
         }
-        in.close();
-        out.close();
     }
 
 
